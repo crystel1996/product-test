c066398fc210459061adec83fc2134f78320e430
crystel1996 <crystelratsimbazafy@gmail.com>
2024-07-20 13:30:07 +0300

    add cors options

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
+import cors from 'cors';
 import routes from './Routes/v1/Route';
 
 const app = express();
@@ -8,6 +10,22 @@ dotenv.config();
 
 const port = process.env.PORT || 3000;
 
+// set security HTTP headers
+app.use(helmet());
+
+// parse json request body
+app.use(express.json());
+
+// Cors options
+const corsOptions = {
+  origin:  process.env.PORT,
+  optionsSuccessStatus: 200 // For legacy browser support
+};
+
+app.use(cors(corsOptions));
+app.use(cors());
+
+// api routes
 app.use('/api', routes);
 
 app.listen(port, () => {
