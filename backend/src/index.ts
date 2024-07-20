f84796baa3910cc1e36c253f17203c308fe77e71
crystel1996 <crystelratsimbazafy@gmail.com>
2024-07-20 11:42:09 +0300

    init backend

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript with Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
